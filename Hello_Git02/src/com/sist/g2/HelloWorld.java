b5e81ed131023972902848ea13a701bd58a0651d
edwinar <dev.edwinner@gmail.com>
2017-09-14 16:09:47 +0900

    1

--- Hello_Git02/src/com/sist/g2/HelloWorld.java
+++ Hello_Git02/src/com/sist/g2/HelloWorld.java
@@ -7,9 +7,12 @@ public class HelloWorld {
 		System.out.println("안녕하세요");
 		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
 		
-		System.out.println("정현준은 짱입니다.");	
-		System.out.println("정현준짱");		
-		System.out.println("정현준 갓 오브 이디엠");
+		System.out.println("정현준짱");
+		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
+		System.out.println("정현준짱");
+		System.out.println("정현준짱");
+		System.out.println("정현준은 짱입니다.");
+		
 	}
 
 }
