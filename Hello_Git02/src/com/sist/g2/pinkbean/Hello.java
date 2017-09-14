2648ba5163074333082a3274a06601dc6efd6364
sist <sist@sist>
2017-09-14 14:35:23 +0900

    dd

--- Hello_Git02/src/com/sist/g2/pinkbean/Hello.java
+++ Hello_Git02/src/com/sist/g2/pinkbean/Hello.java
@@ -1,7 +1,7 @@
 package com.sist.g2.pinkbean;
 
 public class Hello {
-
+//냐라랑
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello222");
--- Hello_Git02/src/com/sist/g2/pinkbean/Pinkbean.java
+++ Hello_Git02/src/com/sist/g2/pinkbean/Pinkbean.java
@@ -1,7 +1,7 @@
 package com.sist.g2.pinkbean;
 
 public class Pinkbean {
-	//이슬기
+	//이슬기2
 	public static void main(String[] args) {
 		// ~(ㅇㅅㅇ )~
 		// ~( ^ㅅ^)~
