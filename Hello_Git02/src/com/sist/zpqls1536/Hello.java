cece50c19cc8c4399b847a991d9a0b9d66be074e
zpqls1536 <zpqls1536@naver.com>
2017-09-14 14:53:00 +0900

    dg

--- Hello_Git02/src/com/sist/zpqls1536/Hello.java
+++ Hello_Git02/src/com/sist/zpqls1536/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello222");
-		System.out.println("Hello2");
+		System.out.println("Hello2"); 
 	}
 
 }
