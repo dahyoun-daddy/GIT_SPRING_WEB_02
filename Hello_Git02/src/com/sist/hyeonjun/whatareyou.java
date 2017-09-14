6d4e0cd996a224ba5aaa5dd92c33e884035fb8fa
edwinar <dev.edwinner@gmail.com>
2017-09-14 15:43:28 +0900

    지우고 커밋

--- Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
+++ Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
@@ -5,12 +5,6 @@ public class whatareyou {
 		System.out.println("뭐하는거야");
 		System.out.println("121");
 		System.out.println("야거는하뭐");
-		System.out.println("육육육육육");
-		System.out.println("오오오오오");
-		System.out.println("우우우우우");
-		System.out.println("칠칠칠칠칠");
-		System.out.println("시스아웃할까요?");		
-		System.out.println("커밋");
 		//
 	}
 }
