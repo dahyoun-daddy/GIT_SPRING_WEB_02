ae7d40fd51c47d190f53633a07cae071ca2cbb9d
edwinar <dev.edwinner@gmail.com>
2017-09-14 15:40:56 +0900

    꺠진거 수정

--- Hello_Git02/src/com/sist/g2/edwinar/HelloMinSeok.java
+++ Hello_Git02/src/com/sist/g2/edwinar/HelloMinSeok.java
@@ -7,11 +7,7 @@ public class HelloMinSeok {
 		System.out.println("눈누나나");
 		System.out.println("깨진 파일 브레이크 파일");
 		System.out.println("안녕하세요");
-<<<<<<< HEAD
-		
-=======
 		System.out.println("********");
->>>>>>> branch 'master' of https://github.com/dahyoun-daddy/GIT_SPRING_WEB_02.git
 	}
 
 }
