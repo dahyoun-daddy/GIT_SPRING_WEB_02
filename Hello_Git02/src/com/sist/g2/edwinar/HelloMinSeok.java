a1ffa44bc7375a95faa986aeebd104fc6bc4b157
james <jamesol@naver.com>
2017-09-14 15:32:32 +0900

    fffff

--- Hello_Git02/src/com/sist/g2/edwinar/HelloMinSeok.java
+++ Hello_Git02/src/com/sist/g2/edwinar/HelloMinSeok.java
@@ -3,7 +3,7 @@ package com.sist.g2.edwinar;
 public class HelloMinSeok {
 
 	public static void main(String[] args) {
-		System.out.println("�ȳ� �ڹ�");
+		System.out.println("********");
 		System.out.println("눈누나나");
 		System.out.println("깨진 파일 브레이크 파일");
 		System.out.println("안녕하세요");
