f462c0cdc9b026fcff397bff4ef50de3913b451f
sist_01 <sist_01@SIST>
2017-09-15 09:19:25 +0900

    asdf

--- Hello_Git02/src/com/sist/g2/HelloWorld.java
+++ Hello_Git02/src/com/sist/g2/HelloWorld.java
@@ -3,21 +3,8 @@ package com.sist.g2;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("안녕하세요");
 		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
-		
-		System.out.println("정현준짱");
-		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
-		System.out.println("정현준짱");
-		System.out.println("정현준짱");
-		System.out.println("정현준은 짱입니다.");	
-		System.out.println("정현준짱");		
-		System.out.println("정현준 갓 오브 이디엠");
-		System.out.println("정현준 갓 오브 이디엠");
-		System.out.println("정현준 갓 오브 이디엠");
-		System.out.println("정현준 갓 오브 이디엠");
-		System.out.println("정현준 갓 오브 이디엠");
 	}
 
 }
