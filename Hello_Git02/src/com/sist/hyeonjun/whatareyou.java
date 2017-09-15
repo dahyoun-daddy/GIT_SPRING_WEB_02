d064bbe1fd512d16e02e68130876cb53c5fa7fbe
sist1 <sist1@sist>
2017-09-15 09:24:47 +0900

    싱크로 맞추는중

--- Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
+++ Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
@@ -4,9 +4,9 @@ public class whatareyou {
 	public static void main(String args[]) {
 	
 		System.out.println("뭐하는거야");
-		
 		System.out.println("정현준");
 		
 		System.out.println("정현준 is god");
+		System.out.println("정현준");
 	}
 }
