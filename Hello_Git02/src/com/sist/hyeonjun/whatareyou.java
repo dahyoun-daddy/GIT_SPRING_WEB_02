f7f7bc9557aef4efe221a1ab4e5952f1d056e3ad
edwinar <dev.edwinner@gmail.com>
2017-09-15 09:21:58 +0900

    123123

--- Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
+++ Hello_Git02/src/com/sist/hyeonjun/whatareyou.java
@@ -2,6 +2,9 @@ package com.sist.hyeonjun;
 
 public class whatareyou {
 	public static void main(String args[]) {
-		System.out.println("뭐하는거야");		
+	
+		System.out.println("뭐하는거야");
+		
+		System.out.println("정현준");
 	}
 }
