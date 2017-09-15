ad190fc33e3e0982be5726f73396e9bd7a7a546f
edwinar <dev.edwinner@gmail.com>
2017-09-15 09:21:38 +0900

    1123

--- Hello_Git02/src/com/sist/g2/HelloWorld.java
+++ Hello_Git02/src/com/sist/g2/HelloWorld.java
@@ -5,7 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
-<<<<<<< HEAD
 		
 		System.out.println("정현준짱");
 		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
@@ -15,9 +14,7 @@ public class HelloWorld {
 		System.out.println("♪└(＾ω＾ )┐♫┌( ＾ω＾)┘");
 		System.out.println("정현준짱");
 		System.out.println("정현준짱");
-		System.out.println("정현준은 짱입니다.");
-		
-=======
->>>>>>> branch 'master' of https://github.com/dahyoun-daddy/GIT_SPRING_WEB_02.git
+		System.out.println("정현준은 짱입니다.");	
+
 	}
 }
